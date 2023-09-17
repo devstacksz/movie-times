c011136e03fabb4f0d92a3698efb01b92db71004
Sultan Ullah <Sultan.Ullah@motorolasolutions.com>
2023-09-17 11:46:47 -0700

    Added styles.css support

--- main.go
+++ main.go
@@ -5,7 +5,14 @@ import (
 	"movie-times/render"
 	"movie-times/scrape"
 	"net/http"
+	"os"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
 	movies := scrape.Scrape()
@@ -13,6 +20,13 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, render.RenderTemplate(movies))
+	})
+
+	http.HandleFunc("/styles.css", func(w http.ResponseWriter, r *http.Request) {
+		dat, err := os.ReadFile("styles.css")
+		check(err)
+		w.Header().Set("Content-Type", "text/css")
+		fmt.Fprint(w, string(dat))
 	})
 
 	fmt.Println("Server running on http://localhost:8080")
